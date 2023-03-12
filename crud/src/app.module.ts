3cf8434b0f0ec9772fee0a62fb8ca4a2d4bc863d
melany.estevez <elibibialany@gmail.com>
2023-03-12 04:21:40 -0400

    start fixed

--- crud/src/app.module.ts
+++ crud/src/app.module.ts
@@ -8,22 +8,25 @@ import { UserController } from './user/user.controller';
 import { UserService } from './user/user.service';
 import { UserModule } from './user/user.module';
 import * as process from 'process'
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     EmployeeModule,
-    //forRoot() - Conexão do nest com o banco de dados
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+    }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'nest-crud',
-      synchronize: true, // local only
-      entities: [Employee],
+      host: process.env.NESTJS_HOST,
+      port: Number(process.env.NESTJS_PORT),
+      username: process.env.NESTJS_USERNAME,
+      password: process.env.NESTJS_PASSWORD,
+      database: process.env.NESTJS_DATABASE,
+      synchronize: true,
+      entities: [process.env.NESTJS_ENTITIES],
     }),
-    UserModule
+    UserModule,
   ],
   controllers: [AppController, UserController],
   providers: [AppService, UserService],
